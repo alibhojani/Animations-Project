1591902bf6b47f774a7479bea6101132fb789d13
ali bhojani <ali bhojani>
2016-04-20 22:00:44 -0400

    Added velocity damping

---
+++ Assets/Collision.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Collision : Constraint {
+
+	private float h; //thickness 
+
+	public Collision () { 
+	
+	}
+
+	public void projectConstraint(Vector3[] ps) { 
+			
+	} 
+	public vertex[] getVertices() { 
+		return null;
+	}
+}
+
--- Assets/VertexManager.cs
+++ Assets/VertexManager.cs
@@ -7,16 +7,26 @@ public class VertexManager : MonoBehaviour {
 	vertex[] vertices; 
 	List <Constraint> constraints;
 
-	public float iterations = 20f;
+	public float iterations = 70f;
 	public float gravity = -9.81f; 
-	bool startSimulation = false;
+	public float kDamping = 1f;
 	public bool enableGravity = true; 
+	private bool startSimulation = false;
 	private int rows = 4;
 	private int columns = 9;
+	private List<vertex> triangles; 
+	private SpatialHash spatialHash;
+	private float thickness; 
+
 
 
 	void Start () {
+		spatialHash = new SpatialHash(2); 
 		vertices = GetComponentsInChildren<vertex> ();
+		for (int i = 0; i < vertices.Length; i++) { 
+			spatialHash.Insert(vertices[i].transform.position, vertices[i]);
+		}
+		triangles = new List<vertex>();
 		constraints = new List<Constraint>();
 		GenerateConstraints();
 	}
@@ -59,14 +69,17 @@ public class VertexManager : MonoBehaviour {
 	}
 	IEnumerator positionBasedSimulation () { 
 		CalculateExternalForces();
-		DampVelocity();
+		//DampVelocity();
 
 		Vector3[] p = new Vector3[vertices.Length];
 		for (int i = 0; i < vertices.Length; i++) { 
 			p[i] = vertices[i].transform.position + (vertices[i].velocity * Time.fixedDeltaTime); //fixed delta time debatable.
 		}
 
-		//TODO:generate collision constraints 
+		//generate collision constraints 
+		for (int i = 0; i < vertices.Length; i++) { 
+			GenerateCollisionConstraints(vertices[i].transform.position, p[i]);
+		}
 
 		for (int i = 0; i < iterations; i++) { 
 			for (int j = 0; j < constraints.Count; j++) { 
@@ -76,6 +89,7 @@ public class VertexManager : MonoBehaviour {
 
 		//velocity and position update in accordance with constraint projections 
 		for (int i = 0; i < vertices.Length; i++) { 
+			if (float.IsNaN(p[i].x) || float.IsNaN(p[i].y) || float.IsNaN(p[i].z)) continue;
 			vertices[i].velocity = (p[i] - vertices[i].transform.position)/Time.fixedDeltaTime; //fixed delta time debatable.
 			vertices[i].transform.position = p[i];
 		}
@@ -96,7 +110,58 @@ public class VertexManager : MonoBehaviour {
 
 	// TODO: implement according to paper 
 	void DampVelocity () { 
-		
+		//initialize xcm and mass acc
+		Vector3 xcm = new Vector3();
+		Vector3 vcm = new Vector3(); 
+		float massAccumulator = 0f; 
+
+		for (int i = 0; i < vertices.Length; i++) { 
+			Vector3 tempPos = vertices[i].transform.position;
+			tempPos.Scale(new Vector3(vertices[i].mass, vertices[i].mass, vertices[i].mass));
+			xcm += tempPos;
+			Vector3 tempVel = vertices[i].velocity;
+			tempVel.Scale(new Vector3(vertices[i].mass, vertices[i].mass, vertices[i].mass));
+			vcm += tempVel;
+			massAccumulator += vertices[i].mass;
+		}
+
+		xcm.Scale(new Vector3(1f/massAccumulator, 1f/massAccumulator, 1f/massAccumulator));
+		vcm.Scale(new Vector3(1f/massAccumulator, 1f/massAccumulator, 1f/massAccumulator));
+		if (float.IsNaN(vcm.x) || float.IsNaN(vcm.y) || float.IsNaN(vcm.z)) vcm = new Vector3();
+		Vector3 L = new Vector3();
+		Matrix4x4 I = new Matrix4x4();
+
+		for (int i = 0; i < vertices.Length; i++) { 
+			Vector3 ri = vertices[i].transform.position - xcm;
+			Vector3 mv = vertices[i].velocity;
+			mv.Scale(new Vector3(vertices[i].mass, vertices[i].mass, vertices[i].mass));
+			L += Vector3.Cross(ri, mv);
+			Matrix4x4 riSkewSem = new Matrix4x4();
+			riSkewSem.SetRow(0, new Vector4(0, -ri.z, ri.y, 0));
+			riSkewSem.SetRow(1, new Vector4(ri.z, 0, -ri.x, 0));
+			riSkewSem.SetRow(2, new Vector4(-ri.y, ri.x, 0, 0));
+			riSkewSem.SetRow(3, new Vector4(0, 0, 0, 1));
+			Matrix4x4 riSkewSemT = riSkewSem.transpose;
+			Matrix4x4 mul = riSkewSemT * riSkewSem;
+			for (int j = 0; j < 3; j++) { 
+				Vector4 temp = mul.GetRow(j);
+				temp.Scale(new Vector4(vertices[i].mass, vertices[i].mass, vertices[i].mass));
+				mul.SetRow(j, temp);
+			}
+			for (int j = 0; j < 3; j++) { 
+				Vector4 temp = I.GetRow(j) + mul.GetRow(j);
+				I.SetRow(j, temp);
+			}
+		}
+		Matrix4x4 I_inverse = I.inverse;
+		Vector3 W = I_inverse.MultiplyPoint(L);
+
+		for (int i = 0; i <vertices.Length; i++) { 
+			Vector3 ri = vertices[i].transform.position - xcm;
+			Vector3 deltaVi = vcm + Vector3.Cross(W, ri) - vertices[i].velocity;
+			deltaVi.Scale(new Vector3(kDamping, kDamping, kDamping));
+			vertices[i].velocity += deltaVi;
+		}
 	}
 
 
@@ -124,10 +189,19 @@ public class VertexManager : MonoBehaviour {
 			if (i%columns == (columns-1)) continue;
 			Stretch s = new Stretch(i, i+columns+1, vertices[i], vertices[i + columns+ 1]);
 			Bend b = new Bend(i, i+columns+1, i+1, i+columns, 
-				vertices[i], vertices[i+columns+1], vertices[i+1], vertices[i+columns]);
+			vertices[i], vertices[i+columns+1], vertices[i+1], vertices[i+columns]);
 			constraints.Add(s);
 			constraints.Add(b);
+			//create triangle t1
+			triangles.Add(vertices[i]); triangles.Add(vertices[i + columns+ 1]); triangles.Add(vertices[i+1]);
+			//create triangle t2
+			triangles.Add(vertices[i + columns+ 1]); triangles.Add(vertices[i]); triangles.Add(vertices[i + columns]);	
 		}
+			
+	}
+
+	void GenerateCollisionConstraints (Vector3 currentPos, Vector3 projectedPos) { 
+		ArrayList nearBy = spatialHash.Query(projectedPos); 
 
 	}
 		
